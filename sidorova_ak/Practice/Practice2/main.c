aa578f347282fc697003c6ecffb7a4b1f6eb8475
Alexandra-Sidorova <alexa-sanechka@ya.ru>
2018-10-22 15:01:01 +0300

    Tab and Unicode

--- sidorova_ak/Practice/Practice2/main.c
+++ sidorova_ak/Practice/Practice2/main.c
@@ -3,93 +3,99 @@
 #include <time.h>
 
 void main()
-{	
-	int n, i, pc, a, b, tmp, f, pl, k, bulls, cows;
-	int player[10];
-	srand((unsigned int)time(0));
+{
+    srand((unsigned int)time(0));
+    long long tmp, pc, pl;
+    int i, a, b, f, n, k, bulls, cows;
+    int player[10];
 
-	printf("\n               \"Bulls and cows\"\n");
-	printf("\nCows - the amount of digits in the intended number that do not stand in their place.");
-	printf("\nBulls - the amount of digits in the intended number that stand in their place.\n");
+    printf("\n               \"Bulls and cows\"\n");
+    printf("\nCows - the amount of digits in the intended number that do not stand in their place.");
+    printf("\nBulls - the amount of digits in the intended number that stand in their place.\n");
 
-	do {                                                                  // Контроль ввода количества цифр числа
-		printf("\nEnter a natural number up to 10 inclusive: ");
-		scanf("%d", &n);
-	} while ((n <= 0) || (n >= 11));
+    do {                                                                  // РљРѕРЅС‚СЂРѕР»СЊ РІРІРѕРґР° РєРѕР»РёС‡РµСЃС‚РІР° С†РёС„СЂ С‡РёСЃР»Р°
+        printf("\nEnter a natural number up to 10 inclusive: ");
+        scanf("%d", &n);
+    } while ((n <= 0) || (n >= 11));
 
-	pc = 0;
-	f = 0;
+    pc = 0;
+    f = 0;
 
-	for (i = 1; i <= n; i++)                                               // Создание рандомного числа компьютером 
-	{                                                                      // без повторяющихся цифр
-		a = rand() % 10;
-		tmp = pc;
+    for (i = 1; i <= n; i++)                                               // РЎРѕР·РґР°РЅРёРµ СЂР°РЅРґРѕРјРЅРѕРіРѕ С‡РёСЃР»Р° РєРѕРјРїСЊСЋС‚РµСЂРѕРј 
+    {                                                                      // Р±РµР· РїРѕРІС‚РѕСЂСЏСЋС‰РёС…СЃСЏ С†РёС„СЂ
+        a = rand() % 10;
+        tmp = pc;
 
-		while (tmp > 0)                                                    // Сама проверка на повторы с флажком
-		{
-			b = tmp % 10;
-			tmp /= 10;
-			if (b == a) f = 1;
-		}
-		if ((a == 0) && (i == 1)) f = 1;                                   // Флажок на тот случай, если первая цифра окажется нулем
-		if (f != 1) pc = pc * 10 + a;                                      // Создание числа, если флажок "опущен"
-		else i--;
-		f = 0;
-	}
+        while (tmp > 0)                                                    // РЎР°РјР° РїСЂРѕРІРµСЂРєР° РЅР° РїРѕРІС‚РѕСЂС‹ СЃ С„Р»Р°Р¶РєРѕРј
+        {
+            b = tmp % 10;
+            tmp /= 10;
+            if (b == a) f = 1;
+        }
 
-	printf("\nWell, I have conceived a number. Try to guess :)\n");
+        if ((a == 0) && (i == 1)) f = 1;                                   // Р¤Р»Р°Р¶РѕРє РЅР° С‚РѕС‚ СЃР»СѓС‡Р°Р№, РµСЃР»Рё РїРµСЂРІР°СЏ С†РёС„СЂР° РѕРєР°Р¶РµС‚СЃСЏ РЅСѓР»РµРј
+        if (f != 1) pc = pc * 10 + a;                                      // РЎРѕР·РґР°РЅРёРµ С‡РёСЃР»Р°, РµСЃР»Рё С„Р»Р°Р¶РѕРє "РѕРїСѓС‰РµРЅ"
+        else i--;
 
-	do {                                                                       // Пошел цикл на обработку вводного числа и его сравнения с задуманным
-		do {
-			f = i = k = 0;
+        f = 0;
+    }
 
-			printf("\nEnter a natural number of non-repeating %d digits: ", n);
-			scanf("%d", &pl);
+    printf("\nWell, I have conceived a number. Try to guess :)\n");
 
-			while (pl > 0)                                                     // Каждую цифру в каждый элемент массива
-			{
-				player[i] = pl % 10;
-				pl /= 10;
-				i++;
-			}
-			if (i == n)                                                        // Отсев, если количество цифр в числе пользователя
-			{                                                                  // не совпадает с количеством цифр числа компьютера (Проверка)
-				for (k = 0; k < n - 1; k++)                                    // Цикл в цикле - проверка на повторы цифр
-				{                                                              // Цикл в цикле, который тоже в цикле - нерационально, понимаю
-					for (i = k + 1; i < n; i++)
-					{
-						if (player[k] == player[i])
-						{
-							f = 1;
-							break;
-						}
+    do {                                                                       // РџРѕС€РµР» С†РёРєР» РЅР° РѕР±СЂР°Р±РѕС‚РєСѓ РІРІРѕРґРЅРѕРіРѕ С‡РёСЃР»Р° 
+        do {                                                                   // Рё РµРіРѕ СЃСЂР°РІРЅРµРЅРёСЏ СЃ Р·Р°РґСѓРјР°РЅРЅС‹Рј
+            f = i = k = 0;
 
-					}
-				}
-			}
-			else f = 1;
-		} while (f != 0);
+            printf("\nEnter a natural number of non-repeating %d digits: ", n);
+            scanf("%lld", &pl);
 
-		i = k = cows = bulls = 0;
+            while (pl > 0)                                                     // РљР°Р¶РґСѓСЋ С†РёС„СЂСѓ РІ РєР°Р¶РґС‹Р№ СЌР»РµРјРµРЅС‚ РјР°СЃСЃРёРІР°
+            {
+                player[i] = pl % 10;
+                pl /= 10;
+                i++;
+            }
 
-		for (i = 0; i <= n - 1; i++)                                           // Проверка на кол-во коров и быков
-		{
-			tmp = pc;
+            if (i == n)                                                        // РћС‚СЃРµРІ, РµСЃР»Рё РєРѕР»РёС‡РµСЃС‚РІРѕ С†РёС„СЂ РІ С‡РёСЃР»Рµ РїРѕР»СЊР·РѕРІР°С‚РµР»СЏ
+            {                                                                  // РЅРµ СЃРѕРІРїР°РґР°РµС‚ СЃ РєРѕР»РёС‡РµСЃС‚РІРѕРј С†РёС„СЂ С‡РёСЃР»Р° РєРѕРјРїСЊСЋС‚РµСЂР° (РџСЂРѕРІРµСЂРєР°)
+                for (k = 0; k < n - 1; k++)                                    // Р¦РёРєР» РІ С†РёРєР»Рµ - РїСЂРѕРІРµСЂРєР° РЅР° РїРѕРІС‚РѕСЂС‹ С†РёС„СЂ
+                {
+                    for (i = k + 1; i < n; i++)
+                    {
+                        if (player[k] == player[i])
+                        {
+                            f = 1;
+                            break;
+                        }
 
-			while (tmp > 0)                                                    // "Рубим" задуманное число
-			{
-				if (((tmp % 10) == player[i]) && (i == k)) bulls++;
-				if (((tmp % 10) == player[i]) && (i != k)) cows++;
-				k++;
-				tmp /= 10;
-			}
-			k = 0;
-		}
+                    }
+                }
+            }
+            else f = 1;
+        } while (f != 0);
 
-		printf("Cows = %d\n", cows);
-		printf("Bulls = %d\n", bulls);
+        i = k = cows = bulls = 0;
 
-	} while (bulls != n);
+        for (i = 0; i <= n - 1; i++)                                           // РџСЂРѕРІРµСЂРєР° РЅР° РєРѕР»-РІРѕ РєРѕСЂРѕРІ Рё Р±С‹РєРѕРІ
+        {
+            tmp = pc;
 
-	printf("\nCongratulations! You win!\n");
+            while (tmp > 0)                                                    // "Р СѓР±РёРј" Р·Р°РґСѓРјР°РЅРЅРѕРµ С‡РёСЃР»Рѕ
+            {
+                if (((tmp % 10) == player[i]) && (i == k)) bulls++;
+                if (((tmp % 10) == player[i]) && (i != k)) cows++;
+
+                k++;
+                tmp /= 10;
+            }
+
+            k = 0;
+        }
+
+        printf("Cows = %d\n", cows);
+        printf("Bulls = %d\n", bulls);
+
+    } while (bulls != n);
+
+    printf("\nCongratulations! You win!\n");
 }
