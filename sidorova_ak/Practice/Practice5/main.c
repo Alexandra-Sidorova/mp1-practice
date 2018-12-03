d4de34b7ed16a058dfce538e9e1dca63e8fe7e14
Alexandra-Sidorova <alexa-sanechka@ya.ru>
2018-12-03 20:49:47 +0300

    Add quick and merge sorting

--- sidorova_ak/Practice/Practice5/main.c
+++ sidorova_ak/Practice/Practice5/main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <malloc.h>
 
 void InputArray(float array[], int n)
 {
@@ -13,18 +14,57 @@ void OutputArray(float array[], int n)
     int i = 0;
 
     for (i; i < n; i++)
-        printf("%f ", array[i]);
+        printf("%.2f ", array[i]);
 }
 
-void Swap(float a, float b)
+void Swap(float *a, float *b)
 {
     float tmp;
-    tmp = a;
-    a = b;
-    b = tmp;
+
+    tmp = *a;
+    *a = *b;
+    *b = tmp;
 }
 
-void SelectionSort(float array[], int n, char sort)
+void QuickSplit(float array[], int *i, int *j, int mid)
+{
+    do 
+    {
+        while (array[*i] < mid)
+            (*i)++;
+        while (array[*j] > mid)
+            (*j)--;
+
+        if (*i <= *j)
+        {
+
+            Swap(&(array[*i]), &(array[*j]));
+
+            (*i)++;
+            (*j)--;
+        }
+    } while (*i <= *j);
+}
+
+void Merge(float array[], int first, int midIndex, int last)
+{
+    int i, j, step;
+    float *tmp = (float*)malloc((last - first + 1) * sizeof(float));
+
+    i = first;
+    j = midIndex + 1;
+
+    for (step = 0; step < last - first + 1; step++)
+        if ((j > last) || ((i <= midIndex) && (array[i] < array[j])))
+            tmp[step] = array[i++];
+        else
+            tmp[step] = array[j++];
+
+    for (step = first; step < last + 1; step++)
+        array[step] = tmp[step - first];
+}
+
+void SelectionSort(float array[], int n, int sort)
 {
     int i = 0, j, minIndex;
     float min;
@@ -33,7 +73,7 @@ void SelectionSort(float array[], int n, char sort)
     {
         min = array[i];
         minIndex = i;
-        
+
         for (j = i + 1; j < n; j++)
         {
             if (array[j] < min)
@@ -48,7 +88,7 @@ void SelectionSort(float array[], int n, char sort)
     }
 }
 
-void InsertionSort(float array[], int n, char sort)
+void InsertionSort(float array[], int n, int sort)
 {
     int i = 1, j;
     float tmp;
@@ -60,28 +100,23 @@ void InsertionSort(float array[], int n, char sort)
 
         while ((j >= 0) && (array[j] > tmp))
         {
-            array[j + 1] = array[j];
-            array[j] = tmp;
+            Swap(&array[j + 1], &array[j]);
             j--;
         }
     }
 }
 
-void BubbleSort(float array[], int n, char sort)
+void BubbleSort(float array[], int n, int sort)
 {
-    int i = 0, j = 1;
+    int i = 0, j;
 
     for (i; i < n; i++)
-    {
-        for (j; j < n - i; j++)
-        {
+        for (j = n - 1; j > i; j--)
             if (array[j - 1] > array[j])
-                Swap(array[j - 1], array[j]);
-        }
-    }
+                Swap(&array[j - 1], &array[j]);
 }
 
-void CountingSort(float array[], int n, char sort)
+void CountingSort(float array[], int n, int sort)
 {
     int i = 0, j = 0, index = 0, min, max;
     int count[100] = { 0 };
@@ -97,7 +132,7 @@ void CountingSort(float array[], int n, char sort)
 
     for (i = 0; i < n; i++)
     {
-        if (array[i] < min) 
+        if (array[i] < min)
             min = array[i];
         if (array[i] > max)
             max = array[i];
@@ -109,7 +144,35 @@ void CountingSort(float array[], int n, char sort)
     for (i = 0; i < (max - min); i++)
         for (j; j < count[i]; j++)
             array[index++] = i;
+}
 
+void QuickSort(float array[], int first, int last, int sort)
+{
+    int midIndex;
+    int i = first;
+    int j = last;
+
+    midIndex = (first + last) / 2;
+
+    QuickSplit(array, &i, &j, array[midIndex]);
+
+    if (j > first) 
+        QuickSort(array, first, j, sort);
+    if (i < last)
+        QuickSort(array, i, last, sort);
+}
+
+void MergeSort(float array[], int first, int last, int sort)
+{
+    int midIndex;
+    midIndex = (last + first) / 2;
+
+    if (first >= last)
+        return;
+
+    MergeSort(array, first, midIndex, 1);
+    MergeSort(array, midIndex + 1, last, 1);
+    Merge(array, first, midIndex, last);
 }
 
 void main()
@@ -121,6 +184,6 @@ void main()
     scanf("%d", &n);
 
     InputArray(array, n);
-    CountingSort(array, n, 'a');
+    QuickSort(array, 0, n - 1, 1);
     OutputArray(array, n);
 }
