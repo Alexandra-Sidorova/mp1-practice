f26e2b9a9fc649d265a74b9dce3c9b3068c6d54f
Alexandra-Sidorova <alexa-sanechka@ya.ru>
2019-04-09 19:18:57 +0300

    tab, space, UTF-8, may be end

--- sidorova_ak/Practice/Practice8/matrix.cpp
+++ sidorova_ak/Practice/Practice8/matrix.cpp
@@ -4,255 +4,228 @@
 
 Matrix::Matrix()
 {
-	rows = 5;
-	cols = 5;
+    rows = 5;
+    cols = 5;
 
-	cells = (double*)malloc(rows * cols * sizeof(double));
-	for (int i = 0; i < rows * cols; i++)
-		cells[i] = 0;
+    cells = (double*)malloc(rows * cols * sizeof(double));
+    for (int i = 0; i < rows * cols; i++)
+        cells[i] = 0;
 }
 
 Matrix::Matrix(int _rows, int _cols)
 {
-	if ((_rows <= 0) || (_cols <= 0))
-		throw Exception("Not correct size of matrix!");
+    if ((_rows <= 0) || (_cols <= 0))
+        throw Exception("Not correct size of matrix!");
 
-	rows = _rows;
-	cols = _cols;
+    rows = _rows;
+    cols = _cols;
 
-	cells = (double*)malloc(rows * cols * sizeof(double));
-	for (int i = 0; i < rows * cols; i++)
-		cells[i] = 0;
+    cells = (double*)malloc(rows * cols * sizeof(double));
+    for (int i = 0; i < rows * cols; i++)
+        cells[i] = 0;
 }
 
 Matrix::Matrix(int _rows, int _cols, double* _cells)
 {
-	if (_cells == NULL)
-		throw Exception("Empty array!");
+    if (_cells == NULL)
+        throw Exception("Empty array!");
 
-	if ((_rows <= 0) || (_cols <= 0))
-		throw Exception("Not correct size of matrix!");
+    if ((_rows <= 0) || (_cols <= 0))
+        throw Exception("Not correct size of matrix!");
 
-	rows = _rows;
-	cols = _cols;
+    rows = _rows;
+    cols = _cols;
 
-	cells = (double*)malloc(rows * cols * sizeof(double));
-	for (int i = 0; i < rows * cols; i++)
-		cells[i] = _cells[i];
+    cells = (double*)malloc(rows * cols * sizeof(double));
+    for (int i = 0; i < rows * cols; i++)
+        cells[i] = _cells[i];
 }
 
 Matrix::Matrix(const Matrix& _matrix)
 {
-	rows = _matrix.rows;
-	cols = _matrix.cols;
+    rows = _matrix.rows;
+    cols = _matrix.cols;
 
-	cells = (double*)malloc(rows * cols * sizeof(double));
-	for (int i = 0; i < cols * rows; i++)
-		cells[i] = _matrix.cells[i];
+    cells = (double*)malloc(rows * cols * sizeof(double));
+    for (int i = 0; i < cols * rows; i++)
+        cells[i] = _matrix.cells[i];
 }
 
 Matrix::~Matrix()
 {
-	rows = 0;
-	cols = 0;
-	delete[] cells;
+    rows = 0;
+    cols = 0;
+    delete[] cells;
 }
 
 const Matrix& Matrix::operator=(const Matrix& _matrix)
 {
-	if (this == &_matrix)
-		return *this;
+    if (this == &_matrix)
+        return *this;
 
-	rows = _matrix.rows;
-	cols = _matrix.cols;
-	
-	delete[] cells;
-	cells = (double*)malloc(rows * cols * sizeof(double));
-	for (int i = 0; i < cols * rows; i++)
-		cells[i] = _matrix.cells[i];
+    rows = _matrix.rows;
+    cols = _matrix.cols;
+    
+    delete[] cells;
+    cells = (double*)malloc(rows * cols * sizeof(double));
+    for (int i = 0; i < cols * rows; i++)
+        cells[i] = _matrix.cells[i];
 
-	return *this;
+    return *this;
 }
 
 Matrix Matrix::operator+(const Matrix& _add)
 {
-	if ((rows != _add.rows) || (cols != _add.cols))
-		throw Exception("Not correct size of matrix!");
+    if ((rows != _add.rows) || (cols != _add.cols))
+        throw Exception("Not correct size of matrix!");
 
-	Matrix result(rows, cols);
+    Matrix result(rows, cols);
 
-	for (int i = 0; i < rows * cols; i++)
-		result.cells[i] += (cells[i] + _add.cells[i]);
+    for (int i = 0; i < rows * cols; i++)
+        result.cells[i] += (cells[i] + _add.cells[i]);
 
-	return result;
+    return result;
 }
 
 Matrix Matrix::operator+(double num)
 {
-	Matrix result(rows, cols);
+    Matrix result(rows, cols);
 
-	for (int i = 0; i < rows * cols; i++)
-		result.cells[i] += (cells[i] + num);
+    for (int i = 0; i < rows * cols; i++)
+        result.cells[i] += (cells[i] + num);
 
-	return result;
+    return result;
 }
 
 Matrix Matrix::operator-(const Matrix& _sub)
 {
-	if ((rows != _sub.rows) || (cols != _sub.cols))
-		throw Exception("Not correct size of matrix!");
+    if ((rows != _sub.rows) || (cols != _sub.cols))
+        throw Exception("Not correct size of matrix!");
 
-	Matrix result(rows, cols);
+    Matrix result(rows, cols);
 
-	for (int i = 0; i < rows * cols; i++)
-		result.cells[i] += (cells[i] - _sub.cells[i]);
+    for (int i = 0; i < rows * cols; i++)
+        result.cells[i] += (cells[i] - _sub.cells[i]);
 
-	return result;
+    return result;
 }
 
 Matrix Matrix::operator-(double num)
 {
-	Matrix result(rows, cols);
+    Matrix result(rows, cols);
 
-	for (int i = 0; i < rows * cols; i++)
-		result.cells[i] += (cells[i] - num);
+    for (int i = 0; i < rows * cols; i++)
+        result.cells[i] += (cells[i] - num);
 
-	return result;
+    return result;
 }
 
 Matrix Matrix::operator+=(const Matrix& _add)
 {
-	if ((rows != _add.rows) || (cols != _add.cols))
-		throw Exception("Not correct size of matrix!");
+    if ((rows != _add.rows) || (cols != _add.cols))
+        throw Exception("Not correct size of matrix!");
 
-	for (int i = 0; i < rows * cols; i++)
-		cells[i] += _add.cells[i];
+    for (int i = 0; i < rows * cols; i++)
+        cells[i] += _add.cells[i];
 
-	return *this;
+    return *this;
 }
 
 Matrix Matrix::operator+=(double num)
 {
-	for (int i = 0; i < rows * cols; i++)
-		cells[i] += num;
+    for (int i = 0; i < rows * cols; i++)
+        cells[i] += num;
 
-		return *this;
+        return *this;
 }
 
 Matrix Matrix::operator-=(const Matrix& _add)
 {
-	if ((rows != _add.rows) || (cols != _add.cols))
-		throw Exception("Not correct size of matrix!");
+    if ((rows != _add.rows) || (cols != _add.cols))
+        throw Exception("Not correct size of matrix!");
 
-	for (int i = 0; i < rows * cols; i++)
-		cells[i] -= _add.cells[i];
+    for (int i = 0; i < rows * cols; i++)
+        cells[i] -= _add.cells[i];
 
-	return *this;
+    return *this;
 }
 
 Matrix Matrix::operator-=(double num)
 {
-	for (int i = 0; i < rows * cols; i++)
-		cells[i] -= num;
+    for (int i = 0; i < rows * cols; i++)
+        cells[i] -= num;
 
-	return *this;
+    return *this;
 }
 
 Matrix Matrix::operator*=(double num)
 {
-	for (int i = 0; i < rows * cols; i++)
-		cells[i] *= num;
+    for (int i = 0; i < rows * cols; i++)
+        cells[i] *= num;
 
-	return *this;
+    return *this;
 }
 
 Matrix Matrix::operator*(const Matrix& _factor)
 {
-	if (cols != _factor.rows)
-		throw Exception("Not correct size of matrix!");
+    if (cols != _factor.rows)
+        throw Exception("Not correct size of matrix!");
 
-	Matrix result(rows, _factor.cols);
+    Matrix result(rows, _factor.cols);
 
-	for (int i = 0; i < rows; i++)
-		for (int j = 0; j < _factor.cols; j++)
-			for (int k = 0; k < cols; k++)
-				result.cells[i * _factor.cols + j] += 
-				(cells[i * cols + k] * _factor.cells[k * _factor.cols + j]);
-		
-	return result;
+    for (int i = 0; i < rows; i++)
+        for (int j = 0; j < _factor.cols; j++)
+            for (int k = 0; k < cols; k++)
+                result.cells[i * _factor.cols + j] += 
+                (cells[i * cols + k] * _factor.cells[k * _factor.cols + j]);
+        
+    return result;
 }
 
 Matrix Matrix::operator*(double num)
 {
-	Matrix result(rows, cols);
+    Matrix result(rows, cols);
 
-	for (int i = 0; i < rows * cols; i++)
-		result.cells[i] += (cells[i] * num);
+    for (int i = 0; i < rows * cols; i++)
+        result.cells[i] += (cells[i] * num);
 
-	return result;
-}
-
-Matrix Matrix::operator~()
-{
-	if (rows != cols)
-		throw Exception("Not correct size of matrix!");
-
-	Matrix E(rows, rows), tmp(*this);
-
-	for (int i = 0; i < (rows * cols); i++)
-		if (i % (cols + 1) == 0)
-			E.cells[i] = 1;
-
-	for (int i = 0; i < tmp.cols; i++)
-	{
-		double key = tmp[i][i];
-
-		for (int j = 0; j < tmp.rows; j++)
-			for (int k = 0; k < tmp.cols; k++)
-			{
-				if (i == j)
-					tmp[i][k] /= key;
-				else
-
-			}
-		}
-	}
+    return result;
 }
 
 double* Matrix::operator[](int index_rows)
 {
-	if ((index_rows < 0) || (index_rows >= rows))
-		throw Exception("Not correct index!");
+    if ((index_rows < 0) || (index_rows >= rows))
+        throw Exception("Not correct index!");
 
-	return(cells + cols * index_rows);
+    return(cells + cols * index_rows);
 }
 
 istream& operator>>(istream& in, Matrix& _matrix)
 {
-	for (int i = 0; i < (_matrix.rows * _matrix.cols); i++)
-		in >> _matrix.cells[i];
+    for (int i = 0; i < (_matrix.rows * _matrix.cols); i++)
+        in >> _matrix.cells[i];
 
-	return in;
+    return in;
 }
 
 ostream& operator<<(ostream &out, const Matrix& _matrix)
 {
-	if ((_matrix.rows * _matrix.cols) == 0)
-	{
-		out << "Empty matrix.";
-		return out;
-	}
+    if ((_matrix.rows * _matrix.cols) == 0)
+    {
+        out << "Empty matrix.";
+        return out;
+    }
 
-	for (int i = 0; i < (_matrix.rows * _matrix.cols); i++)
-	{
-		if (i % _matrix.cols == 0)
-			out << "| ";
+    for (int i = 0; i < (_matrix.rows * _matrix.cols); i++)
+    {
+        if (i % _matrix.cols == 0)
+            out << "| ";
 
-		out << _matrix.cells[i] << " ";
+        out << _matrix.cells[i] << " ";
 
-		if ((i + 1) % _matrix.cols == 0)
-			out << "|" << endl;
-	}
+        if ((i + 1) % _matrix.cols == 0)
+            out << "|" << endl;
+    }
 
-	return out;
+    return out;
 }
--- sidorova_ak/Practice/Practice8/matrix.h
+++ sidorova_ak/Practice/Practice8/matrix.h
@@ -7,34 +7,32 @@ using namespace std;
 class Matrix
 {
 private:
-	int rows; // строки
-	int cols; // столбцы
-	double* cells;
+    int rows; // СЃС‚СЂРѕРєРё
+    int cols; // СЃС‚РѕР»Р±С†С‹
+    double* cells;
 public:
-	Matrix();
-	Matrix(int, int);
-	Matrix(int, int, double*);
-	Matrix(const Matrix&);
-	~Matrix();
+    Matrix();
+    Matrix(int, int);
+    Matrix(int, int, double*);
+    Matrix(const Matrix&);
+    ~Matrix();
 
-	const Matrix& operator=(const Matrix&);
-	Matrix operator+(const Matrix&);
-	Matrix operator+(double);
-	Matrix operator-(const Matrix&);
-	Matrix operator-(double);
-	Matrix operator*(const Matrix&);
-	Matrix operator*(double);
-	Matrix operator+=(const Matrix&);
-	Matrix operator+=(double num);
-	Matrix operator-=(const Matrix&);
-	Matrix operator-=(double num);
-	Matrix operator*=(double num);
-	Matrix operator~();
-	double* operator[](int index_rows);
+    const Matrix& operator=(const Matrix&);
+    Matrix operator+(const Matrix&);
+    Matrix operator+(double);
+    Matrix operator-(const Matrix&);
+    Matrix operator-(double);
+    Matrix operator*(const Matrix&);
+    Matrix operator*(double);
+    Matrix operator+=(const Matrix&);
+    Matrix operator+=(double num);
+    Matrix operator-=(const Matrix&);
+    Matrix operator-=(double num);
+    Matrix operator*=(double num);
+    double* operator[](int index_rows);
 
-	friend istream& operator>> (istream&, Matrix&); // Ввод
-	friend ostream& operator<< (ostream&, const Matrix&); // Вывод
+    friend istream& operator>> (istream&, Matrix&); // Р’РІРѕРґ
+    friend ostream& operator<< (ostream&, const Matrix&); // Р’С‹РІРѕРґ
 };
-
 
 #endif
